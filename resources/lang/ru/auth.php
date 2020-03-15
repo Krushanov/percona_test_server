c5fc90e984acace3c55b07157f43d2f325936867
Alexander Krushanov <a.i.krushanov@gmail.com>
2020-03-15 19:18:36 +0300

    Переведены страницы авторизации пользователя. Реализован Seeder для генерации тестовых значений.

---
+++ resources/lang/ru/auth.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'Login' => 'Вход',
+    'Register' => 'Регистрация',
+    'E-Mail Address' => 'E-Mail Адрес',
+    'Password' => 'Пароль',
+    'Remember Me' => 'Запомнить меня',
+    'Forgot Your Password?' => 'Забыли пароль?',
+    'Reset Password' => 'Сброс пароля',
+    'Send Password Reset Link' => 'Отправить ссылку для сброса пароля',
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
